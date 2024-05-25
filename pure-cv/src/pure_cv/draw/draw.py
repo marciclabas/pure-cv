df35f73b8fd04c72024f6c5f83d481ff9d0e478a
m4rs <marcel@marcelclaramunt.cat>
2024-05-25 17:54:12 +0200

    draw typings

--- pure-cv/src/pure_cv/draw/draw.py
+++ pure-cv/src/pure_cv/draw/draw.py
@@ -14,28 +14,42 @@ def vertices(img: Img, /, vertices: Vertices, *, radius: int = 10, color: int | 
 def vertices(*, vertices: Vertices, radius: int = 10, color: int | tuple[int, int, int] = (255, 0, 0)) -> Callable[[Img], Img]: ...
 
 @R.curry
-def vertices(
-    img: Img, /, vertices: Vertices, radius: int = 10, color: int | tuple[int, int, int] = (255, 0, 0)
-) -> Img: # type: ignore
+def vertices( # type: ignore
+  img: Img, /, vertices: Vertices, radius: int = 10, color: int | tuple[int, int, int] = (255, 0, 0)
+) -> Img:
     if isinstance(color, tuple):
         img = cv.cvtColor(img, cv.COLOR_GRAY2BGR) if len(np.shape(img)) == 2 else np.copy(img)
     output = img.copy()
     for [x, y] in vertices:
-        cv.circle(output, (int(x), int(y)), radius=radius, color=color, thickness=-1)
+        cv.circle(output, (int(x), int(y)), radius=radius, color=color, thickness=-1) # type: ignore
     return output
 
 
-@R.curry
-def vlines(xs: list[float], img: Img, color = (255, 0, 0), thickness = 3):
-  height = img.shape[0]
-  ls = np.array([[[x, 0, x, height-1]] for x in xs], dtype=np.int32)
-  return lines(ls, img, color, thickness)
+@overload
+def vlines(img: Img, /, xs: list[float], *, color: int | tuple[int, int, int] = (255, 0, 0), thickness: int = 3) -> Img: ...
+@overload
+def vlines(*, xs: list[float], color: int | tuple[int, int, int] = (255, 0, 0), thickness: int = 3) -> Callable[[Img], Img]: ...
 
 @R.curry
-def hlines(ys: list[float], img: Img, color = (255, 0, 0), thickness = 3):
-  width = img.shape[1]
-  ls = np.array([[[0, y, width-1, y]] for y in ys], dtype=np.int32)
-  return lines(ls, img, color, thickness)
+def vlines( # type: ignore
+    img: Img, /, xs: list[float], color: int | tuple[int, int, int] = (255, 0, 0), thickness: int = 3
+) -> Img:
+    height = img.shape[0]
+    ls = np.array([[[x, 0, x, height-1]] for x in xs], dtype=np.int32)
+    return lines(img, ls, color=color, thickness=thickness)
+
+@overload
+def hlines(img: Img, /, ys: list[float], *, color: int | tuple[int, int, int] = (255, 0, 0), thickness: int = 3) -> Img: ...
+@overload
+def hlines(*, ys: list[float], color: int | tuple[int, int, int] = (255, 0, 0), thickness: int = 3) -> Callable[[Img], Img]: ...
+
+@R.curry
+def hlines( # type: ignore
+    img: Img, /, ys: list[float], color: int | tuple[int, int, int] = (255, 0, 0), thickness: int = 3
+) -> Img:
+    width = img.shape[1]
+    ls = np.array([[[0, y, width-1, y]] for y in ys], dtype=np.int32)
+    return lines(img, ls, color=color, thickness=thickness)
 
 Segment = Shaped[np.ndarray, '1 4']
 Segments = Shaped[np.ndarray, 'N 1 4'] | Sequence[Segment]
@@ -45,13 +59,13 @@ def lines(img: Img, /, lines: Segments, *, color: int | tuple[int, int, int] = (
 @overload
 def lines(*, lines: Segments, color: int | tuple[int, int, int] = (255, 0, 0), thickness: int = 3) -> Callable[[Img], Img]: ...
 @R.curry
-def lines(
+def lines( # type: ignore
   img: Img, /, lines: Segments, color: int | tuple[int, int, int] = (255, 0, 0), thickness: int = 3
 ) -> Img: # type: ignore
   out = img.copy()
   for line in lines:
     x1, y1, x2, y2 = line[0]
-    cv.line(out, [x1, y1], [x2, y2], color=color, thickness=thickness)
+    cv.line(out, [x1, y1], [x2, y2], color=color, thickness=thickness) # type: ignore
   return out
 
 
@@ -60,13 +74,13 @@ def contours(img: Img, /, contours: Sequence[Shaped[np.ndarray, 'N 2']], *, colo
 @overload
 def contours(*, contours: Sequence[Shaped[np.ndarray, 'N 2']], color: int | tuple[int, int, int] = (255, 0, 0), thickness: int = 3) -> Callable[[Img], Img]: ...
 @R.curry
-def contours(
+def contours( # type: ignore
   img: Img, /, contours: Sequence[Shaped[np.ndarray, 'N 2']], color: int | tuple[int, int, int] = (255, 0, 0), thickness: int = 3
-) -> Img: # type: ignore
+) -> Img:
   if isinstance(color, tuple):
     outimg = cv.cvtColor(img, cv.COLOR_GRAY2BGR) if len(np.shape(img)) == 2 else np.copy(img)
   else:
     outimg = np.copy(img) if len(np.shape(img)) == 2 else cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     
-  return cv.drawContours(outimg, [np.array(c, dtype=np.int32) for c in contours], -1, color, thickness)
+  return cv.drawContours(outimg, [np.array(c, dtype=np.int32) for c in contours], -1, color, thickness) # type: ignore
 
